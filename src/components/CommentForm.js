fb7b3e572156d871ff3c9a2878fc752437fe7317
ajuroshan <ajuaju0483@gmail.com>
2024-09-15 19:09:16 +0530

    feat: cleaned code

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -3,26 +3,13 @@ import axios from 'axios';
 import { Card, Button, Form } from 'react-bootstrap';
 import {UserContext} from "./UserContext";
 import './SearchComponent.css';
+import Cookies from 'js-cookie';
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true
 
-const getCookie = (name) => {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-};
 
 const CommentForm = ({ episodeId, onCommentPosted }) => {
     const [commentText, setCommentText] = useState('');
@@ -41,7 +28,8 @@ const CommentForm = ({ episodeId, onCommentPosted }) => {
         }
 
         setIsSubmitting(true);
-        const csrftoken = getCookie('csrftoken');
+        const csrftoken = Cookies.get('csrftoken');
+
 
         try {
             await axios.post(
